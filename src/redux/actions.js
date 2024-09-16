ed61b688dccf18e58b6e612ff618bd4e3140390f
unknown <royajamal2003@gmail.com>
2024-09-16 03:25:30 -0700

    actions.js file linters error resolved

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,33 +1,37 @@
 export const setBreakLength = (length) => ({
-    type: 'SET_BREAK_LENGTH',
-    payload: length,
-  });
-  
-  export const setSessionLength = (length) => ({
-    type: 'SET_SESSION_LENGTH',
-    payload: length,
-  });
-  
-  export const startCountdown = (intervalID, progFactor) => ({
-    type: 'START_COUNTDOWN',
-    payload: { intervalID, progFactor },
-  });
-  
-  export const stopCountdown = () => ({
-    type: 'STOP_COUNTDOWN',
-  });
-  
-  export const tick = (time, progress) => ({
-    type: 'TICK',
-    payload: { time, progress },
-  });
-  
-  export const switchClockPhase = (activeType, sessionTime, breakTime, progFactor) => ({
-    type: 'SWITCH_CLOCK_PHASE',
-    payload: { activeType, sessionTime, breakTime, progFactor },
-  });
-  
-  export const reset = () => ({
-    type: 'RESET',
-  });
-  
+  type: 'SET_BREAK_LENGTH',
+  payload: length,
+});
+
+export const setSessionLength = (length) => ({
+  type: 'SET_SESSION_LENGTH',
+  payload: length,
+});
+
+export const startCountdown = (intervalID, progFactor) => ({
+  type: 'START_COUNTDOWN',
+  payload: { intervalID, progFactor },
+});
+
+export const stopCountdown = () => ({
+  type: 'STOP_COUNTDOWN',
+});
+
+export const tick = (time, progress) => ({
+  type: 'TICK',
+  payload: { time, progress },
+});
+
+export const switchClockPhase = (activeType, sessionTime, breakTime, progFactor) => ({
+  type: 'SWITCH_CLOCK_PHASE',
+  payload: {
+    activeType,
+    sessionTime,
+    breakTime,
+    progFactor,
+  },
+});
+
+export const reset = () => ({
+  type: 'RESET',
+});
